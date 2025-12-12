2d44b23d5cad45336e773c69d3f3d712debd74f8
Mxo01 <mariodimodica.01@gmail.com>
2025-12-12 09:27:48 +0100

    projects service

---
+++ src/app/shared/services/projects/projects.service.spec.ts
@@ -0,0 +1,58 @@
+import { TestBed } from "@angular/core/testing";
+import { describe, it, beforeEach, afterEach, expect, vi } from "vitest";
+import { MockProvider } from "ng-mocks";
+import { ProjectsService } from "./projects.service";
+import { MilestoneService } from "../milestone/milestone.service";
+import { Milestone, MilestoneEnum } from "../../models/milestone.model";
+import { of } from "rxjs";
+
+describe("ProjectsService", () => {
+	let service: ProjectsService;
+	let milestoneService: MilestoneService;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [ProjectsService, MockProvider(MilestoneService)]
+		});
+
+		service = TestBed.inject(ProjectsService);
+		milestoneService = TestBed.inject(MilestoneService);
+	});
+
+	afterEach(() => {
+		TestBed.resetTestingModule();
+		vi.clearAllMocks();
+	});
+
+	describe("getProjectsMilestones", () => {
+		it("should return a signal that emits project milestones", () => {
+			// GIVEN
+			const type = MilestoneEnum.PROJECT;
+			const mockMilestones: Milestone[] = [
+				{
+					id: "1",
+					type: type,
+					title: "Portfolio",
+					description: "A project",
+					tags: [],
+					period: "2022",
+					milestoneDate: "",
+					logo: undefined,
+					updates: [],
+					media: [],
+					contributors: [],
+					location: ""
+				}
+			];
+
+			vi.spyOn(milestoneService, "getMilestonesByType").mockReturnValue(of(mockMilestones));
+
+			// WHEN
+			const milestones = TestBed.runInInjectionContext(() => service.getProjectsMilestones());
+
+			// THEN
+			expect(milestoneService.getMilestonesByType).toHaveBeenCalledWith(type);
+			expect(milestones()).toEqual(mockMilestones);
+		});
+	});
+});
